89c2ae23ec573252c93a935562b75d2abfe668eb
AxelArciniega <axelarciniega12@icloud.com>
2023-10-16 16:45:05 -0600

    created recipe model in the front end

---
+++ allSpice.client/src/services/RecipesService.js
@@ -0,0 +1,16 @@
+import { AppState } from "../AppState"
+import { Recipe } from "../models/Recipe"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
+
+
+class RecipesService{
+    async getRecipes(){
+        const res = await api.get('api/recipes')
+        logger.log(res.data)
+        AppState.recipes = res.data.map(r => new Recipe(r))
+    }
+
+}
+
+export const recipesService = new RecipesService()
